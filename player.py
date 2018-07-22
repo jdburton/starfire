0b848df14936774fc5e1992cebb3876bcdbb6d1f
JBURTO2 <jburto2@clemson.edu>
2018-07-22 19:10:40 -0400

    Load all images from file. Play around with the sprite class.

---
+++ player.py
@@ -0,0 +1,18 @@
+import pygame
+
+class Player():
+   
+   def __init__(self):
+   
+      self.starfire_images
+      self.death_images
+      
+      self.starfire_sprite = pygame.sprite.Sprite()
+      
+      self.starfire_sprite_group = pygame.sprite.Group()
+      
+   def loadImages(self):
+      pass
+   
+   def loadDeathImages(self):
+      pass
